16161ccfeec2755a03d9cc74a83ad5e2ef41e7da
Vasyl Pavlyuchenko <vasyl.pavlyuchenko@gmail.com>
2024-10-01 16:07:30 +0300

    done.

--- main.py
+++ main.py
@@ -1,76 +1,65 @@
 from sympy import *
-x = Symbol('x')
 
-# наші рівняння та похідні
 def f(x):
     return (sinh(x) - 12 * tanh(x) - 0.311).evalf()
 
-df = lambdify(x, (sinh(x) - 12 * tanh(x) - 0.311).diff(x))
+#df = lambdify(x, (sinh(x) - 12 * tanh(x) - 0.311).diff(x))
+def df(x):
+    return (cosh(x) - (12 / cosh(x) ** 2)).evalf()
 
-def g(x):
+def ddf(x):
+    return ((24 * sinh(x)) / (cosh(x) ** 3) + sinh(x)).evalf()
+
+def phi(x):
     return (asinh(12 * tanh(x) + 0.311)).evalf()
 
-dg = lambdify(x, (asinh(12 * tanh(x) + 0.311)).diff(x))
+def dphi(x):
+    return ((12 * sech(x) ** 2) / (sqrt(144 * (tanh(x) + 0.0259187) ** 2 + 1))).evalf()
 
-# знаходження початкового інтервалу
-def find_initial_interval():
-    a, b = -10, 10
-    while f(a) * f(b) > 0:
-        a *= 2
-        b *= 2
-    return a, b
-
-def bisection(f, a, b, eps):
-    while (b - a) / 2 > eps:
-        c = (a + b) / 2
-        if f(c) == 0:
-            return c
-        elif f(a) * f(c) < 0:
-            b = c
-        else:
-            a = c
-    return (a + b) / 2
-
-def get_q(a, b):
-    return max(abs(dg(a)), abs(dg(b)))
-
-def is_convergent(q, x0, delta):
-    if (q >= 1):
-        print("умова збіжності 1 не виконується")
-        return false
-    elif (abs(g(x0) - x0) > (1 - q * delta)):
-        print("умова збіжності 2 не виконується")
-        return false
-
-    print("збіжність виконується")
-    return true
-
-def simple_iteration(g, x0, eps, q, max_iter = 1000):
-    roots = []
+def simple_iteration(x0, eps, q):
     x = x0
-    for i in range(max_iter):
-        x_new = g(x)
+    i = 0
+    while True:
+        x_new = phi(x)
+        i += 1
+        print("Ітерація: ", i, "| xn = ", x_new)
         if (q < 0.5):
             if (abs(x_new - x)) <= (((1 - q) / q) * eps):
+                print("Корінь знайдено")
                 return x_new, i
-                #roots.append([x_new, i])
-        elif (abs(x_new - x) <= eps):
+        elif abs(x_new - x) <= eps:
+            print("Корінь знайдено")
             return x_new, i
-            #roots.append([x_new, i])
         x = x_new
-    #return max(roots[0])
 
+def modified_newton(x0, eps):
+    x = x0
+    i = 0
+    dfx0 = df(x0)
+    while True:
+        x_new = x - f(x) / dfx0
+        i += 1
+        print("Ітерація: ", i, "| xn = ", x_new)
+        if (abs(x_new - x) <= eps):
+            print("Корінь знайдено")
+            return x_new, i
+        x = x_new
+
+# Знаходимо початковий інтервал
+a, b = 3, 4
+x0 = 3.5 # Початкове наближення
 eps = 1e-4
-a, b = find_initial_interval()
-x0 = bisection(f, a, b, eps)
 
-# перевірка умов збіжності
-q = get_q(a, b)
-delta = min(abs(a - x0), abs(b - x0))
-is_convergent = is_convergent(q, x0, delta)
+# Метод простої ітерації
+# Перевірка умов збіжності
+q = max(abs(dphi(a)), abs(dphi(b)))
+print("Метод простої ітерації:")
+apriori_simple_iter = ceiling((ln((abs(phi(x0) - x0)) / ((1 - q) * eps))) / (ln(1 / q))) + 1
+print("Апріорна оцінка методу простої ітерації:", apriori_simple_iter)
+simple_iter_result, aposteriori_simple_iter = simple_iteration(x0, eps, q)
+print("Корінь: ", simple_iter_result, "| Апостеріорна оцінка: ", aposteriori_simple_iter)
 
-# апріорна оцінка
-apriori_iter = ceiling((ln((abs(g(x0) - x0)) / ((1 - q) * eps))) / (ln(1 / q))) + 1
-
-print(apriori_iter)
-print(simple_iteration(g, x0, eps, q))
+# Модифікований метод Ньютона
+print ("Модифікований метод Ньютона:")
+modified_newton_result, aposteriori_modified_newton = modified_newton(x0, eps)
+print("Корінь: ", modified_newton_result, "| Апостеріорна оцінка: ", aposteriori_modified_newton)
